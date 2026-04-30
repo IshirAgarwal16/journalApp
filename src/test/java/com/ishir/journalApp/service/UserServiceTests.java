36f56ba3144f64399d563556923e0042c651fc53
IshirAgarwal16 <227200410+IshirAgarwal16@users.noreply.github.com>
2026-05-01 01:36:06 +0530

    Fixed tests using Mockito

--- src/test/java/com/ishir/journalApp/service/UserServiceTests.java
+++ src/test/java/com/ishir/journalApp/service/UserServiceTests.java
@@ -2,49 +2,39 @@ package com.ishir.journalApp.service;
 
 import com.ishir.journalApp.entity.User;
 import com.ishir.journalApp.repository.UserRepository;
-import org.junit.jupiter.api.Disabled;
-import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ArgumentsSource;
-import org.junit.jupiter.params.provider.CsvSource;
-import org.junit.jupiter.params.provider.EnumSource;
-import org.junit.jupiter.params.provider.ValueSource;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import static org.mockito.Mockito.*;
 import static org.junit.jupiter.api.Assertions.*;
 
-@SpringBootTest
+@ExtendWith(MockitoExtension.class)
 public class UserServiceTests {
 
-
-    @Autowired
+    @Mock
     private UserRepository userRepository;
 
-    @Autowired
+    @InjectMocks
     private UserService userService;
-    //@CsvSource
-    // @EnumSource
 
-//    @ValueSource(strings = {
-//            "ram",
-//            "shyam",
-//            "vipul"
-//    })
     @ParameterizedTest
     @ArgumentsSource(UserArgumentProvider.class)
     public void testSaveNewName(User user){
-        assertTrue(userService.saveNewUser(user));
-    }
 
+        // 👉 Mock behavior
+        when(userRepository.save(any(User.class))).thenReturn(user);
 
-    @Disabled
-    @ParameterizedTest
-    @CsvSource({
-            "1,1,2",
-            "2,10,12",
-            "3,3,9"
-    })
-    public void test(int a, int b, int expected){
-        assertEquals(expected,a+b);
+        // 👉 Call method
+        boolean result = userService.saveNewUser(user);
+
+        // 👉 Assert
+        assertTrue(result);
+
+        // 👉 Verify call
+        verify(userRepository, times(1)).save(user);
     }
 }
